fd26494da9d815c44afcb7224dbdf271645e2804
Sutirtho Datta(Sid) <sutirtho10@gmail.com>
2016-08-02 00:21:01 -0700

    PracticeListComp2P2

---
+++ ex10.py
@@ -0,0 +1,29 @@
+# EXERCISE 10 from:
+# http://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
+
+#solution with given inputs
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print a
+print b
+print list(set([i for i in a for j in b if i == j]))
+
+#solution by generating random lists
+import random
+a = []
+b = []
+#Generating two lists with random numbers ranging from 1 to 100
+#with random size of 10-20
+for i in range(random.randint(10,20)):
+    a.append(random.randint(1,100))
+
+for i in range(random.randint(10,20)):
+    b.append(random.randint(1,100))
+
+print"==========================================="
+a.sort()
+b.sort()
+print a
+print b
+print list(set([i for i in a for j in b if i == j]))
+
