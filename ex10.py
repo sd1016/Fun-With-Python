6c3fc3f321dd18d330355fce5605352342dc0c6f
Sutirtho Datta(Sid) <sutirtho10@gmail.com>
2016-08-02 00:22:05 -0700

    PracticeListComp2P2

--- ex10.py
+++ ex10.py
@@ -1,5 +1,5 @@
 # EXERCISE 10 from:
-# http://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
+# http://www.practicepython.org/
 
 #solution with given inputs
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
