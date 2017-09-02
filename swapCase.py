3ffe2e6e44a47ae8cb65953dad0ed6e89189701a
Sutirtho Datta <sutirtho10@gmail.com>
2017-09-02 11:25:41 -0700

    Update swapCase.py
    adding equivalent python string method

--- swapCase.py
+++ swapCase.py
@@ -1,3 +1,4 @@
 # convert all lowercase letters to uppercase letters and vice versa-PYTHON 2.7
 print "".join([i.upper() if i.islower() else i.lower() for i in raw_input()])
+# equivalent to - print s.swapcase()
 
