7fb64b30baeb073ac81a38b982b0e9a3c3939c9c
Sutirtho Datta <sutirtho10@gmail.com>
2017-09-02 13:30:28 -0700

    union, intersection, and difference of two sets

--- un_int_diff_set.py
+++ un_int_diff_set.py
@@ -7,8 +7,10 @@ print "Set A difference Set B = {0}".format(A.difference(B))
 
 """
 OUTPUT:
+
 Set A = set([1, 2, 3, 4, 5])  Set B = set([8, 4, 5, 6, 7])
 Set A union Set B = set([1, 2, 3, 4, 5, 6, 7, 8])
 Set A intersection Set B = set([4, 5])
 Set A difference Set B = set([1, 2, 3])
+
 """
