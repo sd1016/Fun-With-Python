4027d2df40a1a282a2340b96b1d20579210738ba
Sutirtho Datta <sutirtho11@gmail.com>
2019-06-17 14:18:03 +0000

    adding soln to ex23 & modified temp file to ex34

---
+++ ex23.py
@@ -0,0 +1,34 @@
+#Solution to http://www.practicepython.org/exercise/2014/12/14/23-file-overlap.html
+
+#store happy numbers in happylist:
+with open("./temp-files/ex23-happy-nos.txt") as f:
+    happylist = f.readlines()
+
+#store prime numbers in primelist:
+with open("./temp-files/ex23-prime-nos.txt") as f:
+    primeslist = f.readlines()
+
+#intersection of 2 lists
+list3 = [elem.strip() for elem in primeslist if elem in happylist]
+
+print (list3)
+print (len(list3))
+
+
+
+
+def filetolistofints(filename):
+    list_to_return = []
+    with open(filename) as f:
+        line = f.readline()
+        while line:
+            list_to_return.append(int(line))
+            line = f.readline()
+    return list_to_return
+
+primeslist = filetolistofints('./temp-files/ex23-prime-nos.txt')
+happieslist = filetolistofints('./temp-files/ex23-happy-nos.txt')
+
+overlaplist = [elem for elem in primeslist if elem in happieslist]
+print(overlaplist)
+print (len(overlaplist))
