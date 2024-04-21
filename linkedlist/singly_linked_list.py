c55a0f4d983dca47c79d6b2c8cf1dc8f7751ef90
Tirtho <sutirtho11@gmail.com>
2024-04-20 18:17:20 -0700

    initial commit singly linkedlist april 2024

---
+++ linkedlist/singly_linked_list.py
@@ -0,0 +1,18 @@
+class LinkedList:
+    def __init__(self):
+        """
+        Create and initialize LinkedList class instance.
+        >>> linked_list = LinkedList()
+        >>> linked_list.head is None
+        True
+        """
+        self.head = None
+        
+def main():
+
+    linked_list = LinkedList()
+
+if __name__ == "__main__":
+    main()
+
+
